0db01ab634366042a53c7ded1cebeff0b32b80ed
Thinler <69473286+Thinler@users.noreply.github.com>
2024-03-09 21:59:20 +0800

    LCD屏显集成蜂鸣器鸣叫

--- App/Lab1_hmi.c
+++ App/Lab1_hmi.c
@@ -3,28 +3,48 @@
 #include <stdbool.h>
 #include "HSP_TFT18.h"
 
+extern uint32_t sys_tick_counter;
 extern uint8_t RES_value; //extern是外部变量声明，表示该变量在别的文件中定义
 volatile uint8_t lab1_var;  //volatile是声明变量为易失性变量，表示该变量可能会被别的程序修改
 extern volatile uint8_t mode_flag; // 0表示轮询，1表示中断
-// collect rotary encoder switch signal by polling
 
-// uint8_t state_pha, state_phb;
-// 	uint8_t state_pha_t, state_phb_t;
+void Lab1_res_polling()
+{
+	uint8_t state_pha, state_phb;
+	uint8_t state_pha_t, state_phb_t;
+	uint8_t pulse_counter = 0;
 	
-// 	state_pha = PHA2(),			state_phb = PHB2();
-// 	state_pha_t = state_pha,	state_phb_t = state_phb;
-
-
-// void Lab1_res_polling()
-// {
+	state_pha = PHA2(),			state_phb = PHB2();
+	state_pha_t = state_pha,	state_phb_t = state_phb;
 	
-	
-// }
+	while(1)
+	{
+		state_pha = PHA2();
+		state_phb = PHB2();
+		if(state_pha != state_pha_t)	// PHA2 changed
+		{
+			if(SET == state_pha)
+			{
+				if(RESET == state_phb)	// 实测逻辑关系
+				{
+					pulse_counter++;
+				}
+				else if(pulse_counter>0)
+				{
+					pulse_counter--;
+				}
+			}
+			state_pha_t = state_pha;
+			state_phb_t = state_phb;
+		}
+		hsp_cat9555_seg7_decimal(pulse_counter);
+		
+	}
+}
 
-// collect rotary encoder switch signal by interrupt
 void Lab1_res_interrupt()
 {
-	// uint8_t pulse_counter = 0;
+	uint8_t pulse_counter = 0;
 
     /* PHA2/PB14 interrupt enable */
     rcu_periph_clock_enable(RCU_SYSCFG);
@@ -33,16 +53,19 @@ void Lab1_res_interrupt()
     exti_init(EXTI_14, EXTI_INTERRUPT, EXTI_TRIG_RISING);
     exti_interrupt_flag_clear(EXTI_14);
 
-	lab1_var = RES_value;
-	// hsp_cat9555_seg7_decimal(lab1_var);
-
-		if(RES_value != lab1_var)
+	pulse_counter = RES_value;
+	hsp_cat9555_seg7_decimal(pulse_counter);
+	while(1)
+	{
+		if(RES_value != pulse_counter)
 		{
-			lab1_var = RES_value;
-			hsp_cat9555_seg7_hexadecimal(lab1_var);
+			pulse_counter = RES_value;
+			hsp_cat9555_seg7_decimal(pulse_counter);
 		}
-
+	}
 }
+
+
 
 
 void lab1_lcdshow(uint8_t lab1_var) { 
@@ -65,24 +88,28 @@ void lab1_lcdshow(uint8_t lab1_var) {
 	
 }
 
+
 void Lab1_mainfunc() {
-	
-	
     uint8_t state_pha, state_phb;
 	uint8_t state_pha_t, state_phb_t;
-	uint8_t pulse_counter = 0;
+	// uint8_t pulse_counter = 0;
 	
 	state_pha = PHA2(),			state_phb = PHB2();
 	state_pha_t = state_pha,	state_phb_t = state_phb;
+
+ 	/* PHA2/PB14 interrupt enable */
+	rcu_periph_clock_enable(RCU_SYSCFG);
+    nvic_irq_enable(EXTI10_15_IRQn, 2U, 0U);
+    syscfg_exti_line_config(EXTI_SOURCE_GPIOB, EXTI_SOURCE_PIN14);
+    exti_init(EXTI_14, EXTI_INTERRUPT, EXTI_TRIG_RISING);
+    exti_interrupt_flag_clear(EXTI_14);
 
 	//通过SW1拨码选择轮询或者中断方式
 	while (1)
 	{
 	mode_flag = SW1() ? 1 : 0; // 假设SW1()为高时使用中断，低时使用轮询
 	if (mode_flag == 0) { //轮询
-	while (1)
-	{
-		// lab1_var = 127;
+		
 		state_pha = PHA2();
 		state_phb = PHB2();
 		if(state_pha != state_pha_t)	// PHA2 changed
@@ -139,37 +166,90 @@ void Lab1_mainfunc() {
 			state_pha_t = state_pha;
 			state_phb_t = state_phb;
 		}
-		if(!PUSH()) {
-        lab1_var = 127;
-   	 }
+		
 		lab1_lcdshow(lab1_var);
 		hsp_cat9555_seg7_hexadecimal(lab1_var);
-	}
 
+		if ((lab1_var < 10) || (lab1_var > 50 && lab1_var < 80)) {
+        // 调用蜂鸣器以1秒周期鸣叫0.02s的函数
+			if (sys_tick_counter >= 1000)
+			{
+			BUZZ_ON();
+			if (sys_tick_counter >= 1020)
+			{
+				BUZZ_OFF();
+				sys_tick_counter = 20;
+			}
+			}	
+    	} else if (lab1_var >= 80) {
+        // 调用蜂鸣器以0.2秒周期鸣叫0.02s的函数
+			if (sys_tick_counter >= 200)
+			{
+			BUZZ_ON();
+			if (sys_tick_counter >= 220)
+			{
+			BUZZ_OFF();
+			sys_tick_counter = 20;
+			}
+			}
+   		 }
+	
+		RES_value = lab1_var;
 	}
 
 	else { //中断
-		Lab1_res_interrupt();
-	}
-	}
-
-
-
-	// 蜂鸣器逻辑
-    	if ((lab1_var < 10) || (lab1_var > 50 && lab1_var < 80)) {
-        // 调用蜂鸣器以1秒周期鸣叫0.02s的函数
+	lab1_var = RES_value;
+	hsp_cat9555_seg7_hexadecimal(lab1_var);
+	
+		if(RES_value != lab1_var)
+		{
+			lab1_var = RES_value;
+			hsp_cat9555_seg7_hexadecimal(lab1_var);
+		}
 		
+		if ((lab1_var < 10) || (lab1_var > 50 && lab1_var < 80)) {
+        // 调用蜂鸣器以1秒周期鸣叫0.02s的函数
+			if (sys_tick_counter >= 1000)
+			{
+			BUZZ_ON();
+			if (sys_tick_counter >= 1020)
+			{
+				BUZZ_OFF();
+				sys_tick_counter = 20;
+			}
+			}	
     	} else if (lab1_var >= 80) {
         // 调用蜂鸣器以0.2秒周期鸣叫0.02s的函数
-
+			if (sys_tick_counter >= 2000)
+			{
+			BUZZ_ON();
+			if (sys_tick_counter >= 2020)
+			{
+			BUZZ_OFF();
+			sys_tick_counter = 20;
+			}
+			}
    		 }
+		 lab1_lcdshow(lab1_var);
+	}
+	
+	if(!PUSH()) {
+        lab1_var = 127;
+		RES_value = 127;
+   	 	}
+	}
+
+
 
     // 更新显示（这需要您根据实际使用的显示设备进行实现）
 		// hsp_cat9555_seg7_hexadecimal(lab1_var);
-	
 
 
 }
+
+
+
+
 
 
 
